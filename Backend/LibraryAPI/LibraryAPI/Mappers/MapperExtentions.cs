78fbfe8c06aa90c14df75df11b776179d83fccf8
DTorosyan <DTorosyan@ebsco.com>
2025-11-15 00:47:43 +0400

    Upgraded from .NET9.0 to .NET 10

--- Backend/LibraryAPI/LibraryAPI/Mappers/MapperExtentions.cs
+++ Backend/LibraryAPI/LibraryAPI/Mappers/MapperExtentions.cs
@@ -5,37 +5,43 @@ namespace LibraryAPI.Mappers;
 
 public static class MapperExtentions
 {
-    public static BookDto ToBookDto(this Book book) =>
-        new()
-        {
-            Id = book.Id,
-            Title = book.Title,
-            CategoryId = book.CategoryId,
-            CategoryName = book.Category.Name,
-            ImageURL = book.ImageURL,
-        };
+    extension(Book book)
+    {
+        public BookDto ToBookDto() =>
+            new()
+            {
+                Id = book.Id,
+                Title = book.Title,
+                CategoryId = book.CategoryId,
+                CategoryName = book.Category.Name,
+                ImageURL = book.ImageURL,
+            };
 
-    public static BookWithDetailsDto ToBookWithDetailsDto(this Book book) =>
-        new()
-        {
-            Id = book.Id,
-            Title = book.Title,
-            ISBN = book.ISBN,
-            Pages = book.Pages,
-            PublishedDate = book.PublishedDate,
-            Description = book.Description,
-            AuthorId = book.AuthorId,
-            AuthorFullName = book.Author.FullName,
-            CategoryId = book.CategoryId,
-            CategoryName = book.Category.Name,
-            BookURL = book.BookURL,
-            ImageURL = book.ImageURL
-        };
+        public BookWithDetailsDto ToBookWithDetailsDto() =>
+            new()
+            {
+                Id = book.Id,
+                Title = book.Title,
+                ISBN = book.ISBN,
+                Pages = book.Pages,
+                PublishedDate = book.PublishedDate,
+                Description = book.Description,
+                AuthorId = book.AuthorId,
+                AuthorFullName = book.Author.FullName,
+                CategoryId = book.CategoryId,
+                CategoryName = book.Category.Name,
+                BookURL = book.BookURL,
+                ImageURL = book.ImageURL
+            };
+    }
 
-    public static CategoryDto ToCategoryDto(this Category category) =>
-        new()
-        {
-            Id = category.Id,
-            Name = category.Name
-        };
+    extension(Category category)
+    {
+        public CategoryDto ToCategoryDto() =>
+            new()
+            {
+                Id = category.Id,
+                Name = category.Name
+            };
+    }
 }
